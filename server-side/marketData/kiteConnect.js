f68f5e9708f6140077ac531c6c24218113d404de
vvv201214 <vvv201214@gmail.com>
2022-12-22 11:59:07 +0530

    update

--- server-side/marketData/kiteConnect.js
+++ server-side/marketData/kiteConnect.js
@@ -13,8 +13,8 @@ let newCors = process.env.NODE_ENV === "production" ? "http://3.110.187.5/" : "h
 const io = new Server(9000, {
   cors: {
 
-    // origin: newCors,
-    origin: "http://3.110.187.5/",
+    origin: newCors,
+    // origin: "http://3.110.187.5/",
 
     methods: ['GET', 'POST', 'PATCH'],
   },
@@ -113,7 +113,7 @@ async function parameters(io, socket) {
       function onTicks(ticks) {
 
         if(token.length === ticks.length){
-          // console.log('Ticks', ticks);
+          console.log('Ticks', ticks);
           socket.emit('tick', ticks);
           // console.log(socket);
         }
