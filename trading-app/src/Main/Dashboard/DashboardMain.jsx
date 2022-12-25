8693cc9a541583ac8e12e60b582da49ceda3b299
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2022-12-25 19:42:54 +0530

    UI Changes

--- trading-app/src/Main/Dashboard/DashboardMain.jsx
+++ trading-app/src/Main/Dashboard/DashboardMain.jsx
@@ -14,8 +14,8 @@ function DashboardMain(){
             <div className="User_header">
             <h1>{`Hello ${getDetails.userDetails.name}! Welcome Back`}</h1>
                 <div className="indexheader">
-                <div className="indexes"><span>Nifty 50</span><span> : </span><span>18,300</span><span> </span><span>+3.45%</span></div>
-                <div className="indexes"><span>Nifty Bank</span><span> : </span><span>43,500</span><span> </span><span>+2.45%</span></div>
+                <div className="indexes"><span>Nifty 50</span><span> : </span><span>NA</span><span> </span><span>NA</span></div>
+                <div className="indexes"><span>Nifty Bank</span><span> : </span><span>NA</span><span> </span><span>NA</span></div>
                 </div>
                 <div className="logo_btn" >ninepointer</div>
             </div>
