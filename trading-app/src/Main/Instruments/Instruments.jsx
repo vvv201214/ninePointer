f36d7303d3fa44eec5d1b3d3af46798595462064
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2023-01-03 19:20:50 +0530

    Change in the grid of Instruments Tab

--- trading-app/src/Main/Instruments/Instruments.jsx
+++ trading-app/src/Main/Instruments/Instruments.jsx
@@ -3,6 +3,7 @@ import Styles from './InstrumentModel.module.css';
 import uniqid from "uniqid";
 import axios from "axios";
 import InstrumentsEditModel from "./InstrumentEditModel/InstrumentsEditModel";
+import EditIcon from '@mui/icons-material/Edit';
 
 
 function Instruments() {
@@ -103,7 +104,7 @@ function Instruments() {
             <div className="main_Container">
                 <div className="right_side">
                     <div className="rightside_maindiv">
-                        <button onClick={toggleModal} className="Ac_btn">Create instrument</button>
+                        <button onClick={toggleModal} class="btnnew bg-gradient-info mt-3 w-15">Create Instrument</button>
                         {modal && (
                             <div className="modal">
                                 <div onClick={toggleModal} className="overlay"></div>
@@ -134,40 +135,82 @@ function Instruments() {
                                 </div>
                             </div>
                         )}
-                        <span className="grid1_span">Active Instruments</span>
-                        <div className="grid_1">
-                            <table className="grid1_table">
-                                <tr className="grid2_tr">
-                                    <th className="grid2_th">Created On</th>
-                                    <th className="grid2_th">Contract Date</th>
-                                    <th className="grid2_th">Instrument</th>
-                                    <th className="grid2_th">Exchange</th>
-                                    <th className="grid2_th">Symbol</th>
-                                    <th className="grid2_th">Lot Size</th>
-                                    <th className="grid2_th">Max Lot</th>
-                                    <th className="grid2_th">Status</th>
-                                    <th className="grid2_th">Last Modified</th>
-                                </tr>
-                                {
-                                    activeData.map((elem) => {
-                                        return (
-                                            <tr className="grid2_tr">
-                                                <td className="grid2_td"><span className="Editbutton"><InstrumentsEditModel data={activeData} id={elem._id} Render={{setReRender, reRender}}/></span>{elem.createdOn}</td>
-                                                <td className="grid2_td">{elem.contractDate}</td>
-                                                <td className="grid2_td">{elem.instrument}</td>
-                                                <td className="grid2_td">{elem.exchange}</td>
-                                                <td className="grid2_td">{elem.symbol}</td>
-                                                <td className="grid2_td">{elem.lotSize}</td>
-                                                <td className="grid2_td">{elem.maxLot}</td>
-                                                <td className="grid2_td">{elem.status}</td>
-                                                <td className="grid2_td">{elem.lastModified}</td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </table>
+
+
+<div class="row">
+        <div class="col-12">
+          <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3">Active Instruments</h6>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Instruments</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Contract Date</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Exchange</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Lot Size</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Max Quantity</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Created On</th>
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead>
+                {
+                    activeData.map((elem) => {
+                    return (
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          {/* <div>
+                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3 border-radius-lg" alt="user1"/>
+                          </div> */}
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">{elem.instrument}</h6>
+                            <p class="text-xs text-secondary mb-0">{elem.symbol}</p>
+                          </div>
                         </div>
-                        <span className="grid2_span">Inactive Instruments</span>
+                      </td>
+                      <td>
+                      <span class="align-middle text-center text-secondary text-xs font-weight-bold">{elem.contractDate}</span>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-success">{elem.exchange}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.lotSize}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.maxLot}</span>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-success">{elem.status}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.createdOn}</span>
+                      </td>
+                      <td class="align-middle">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                        <InstrumentsEditModel data={activeData} id={elem._id} Render={{setReRender, reRender}}/>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                    )}
+                    )}
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+                        {/* <span className="grid2_span">Inactive Instruments</span>
                         <div className="grid_2">
                             <table className="grid1_table">
                                 <tr className="grid2_tr">
@@ -198,7 +241,81 @@ function Instruments() {
                                 })
                                 }
                             </table>
+                        </div> */}
+
+<div class="row">
+        <div class="col-12">
+          <div class="card my-4">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+              <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                <h6 class="text-white text-capitalize ps-3">Active Instruments</h6>
+              </div>
+            </div>
+            <div class="card-body px-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Instruments</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Contract Date</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Exchange</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Lot Size</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Max Quantity</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Created On</th>
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead>
+                {
+                    inactiveData.map((elem) => {
+                    return (
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2 py-1">
+                          {/* <div>
+                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3 border-radius-lg" alt="user1"/>
+                          </div> */}
+                          <div class="d-flex flex-column justify-content-center">
+                            <h6 class="mb-0 text-sm">{elem.instrument}</h6>
+                            <p class="text-xs text-secondary mb-0">{elem.symbol}</p>
+                          </div>
                         </div>
+                      </td>
+                      <td>
+                      <span class="align-middle text-center text-secondary text-xs font-weight-bold">{elem.contractDate}</span>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-success">{elem.exchange}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.lotSize}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.maxLot}</span>
+                      </td>
+                      <td class="align-middle text-center text-sm">
+                        <span class="badge badge-sm bg-gradient-success">{elem.status}</span>
+                      </td>
+                      <td class="align-middle text-center">
+                        <span class="text-secondary text-xs font-weight-bold">{elem.createdOn}</span>
+                      </td>
+                      <td class="align-middle">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                        <InstrumentsEditModel data={inactiveData} id={elem._id} Render={{setReRender, reRender}}/>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                    )}
+                    )}
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
                     </div>
                 </div>
             </div>
--- trading-app/src/Main/TradingAccounts/TradingACMain.js
+++ trading-app/src/Main/TradingAccounts/TradingACMain.js
@@ -5,15 +5,36 @@ import TradingHeader from "./TradingHeader";
 
 function TradingACMain(){
     const getDetails = useContext(userContext);
+
+
     return(
-        <>
-            <div className="User_header">
-                <h1>{`Hello ${getDetails.userDetails.name}! Welcome Back`}</h1>
-                <div className="indexheader">
-                <div className="indexes"><span>Nifty 50</span><span> : </span><span>&nbsp;NA&nbsp;  </span><span> </span><span> NA </span></div>
-                <div className="indexes"><span>Nifty Bank</span><span> : </span><span>&nbsp; NA &nbsp; </span><span> </span><span> NA </span></div>
+        < >
+            <div class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl">
+            <div class="container-fluid py-1 px-3">
+                <div>
+                <nav aria-label="breadcrumb">
+                {/* <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
+                    <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">🟢</a></li>
+                    <li class="breadcrumb-item text-sm text-dark active" aria-current="page"><div className={Styles.blinkitlive}>Live</div></li>
+                </ol> */}
+                <h6 class="font-weight-bolder mb-0">{`Hi ${getDetails.userDetails.name}! Welcome Back`}</h6>
+                </nav>
                 </div>
-                <button className="logo_btn" >ninepointer</button>
+                <div class="mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          <ul class="navbar-nav  justify-content-evenly">
+            <li class="nav-item d-flex align-items-center mx-sm-1">
+                <div class="btnnew bg-gradient-info mt-3 w-200"><span>Nifty 50</span><span> : </span><span>&nbsp;NA&nbsp;  </span><span> </span><span> NA </span></div>
+            </li>
+            <li class="nav-item d-flex align-items-center mx-sm-1">
+                <div class="btnnew bg-gradient-info mt-3 w-200"><span>Nifty Bank</span><span> : </span><span>&nbsp; NA &nbsp; </span><span> </span><span> NA </span></div>
+            </li>
+            <li class="nav-item d-flex align-items-center mx-sm-1">
+                <div class="btnnew bg-gradient-primary mt-3 w-100"><span>ninepointer</span></div>
+                
+            </li>
+            </ul>
+            </div>
+            </div>
             </div>
             <TradingHeader/>
         </>
